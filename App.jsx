15bf9637176bfc538903fa5452214af159933a50
MohamedLiyassudeen <mohamed.m@skava.com>
2019-02-27 15:32:31 +0530

    Display list of elements and obtaining strikeout counts

--- App.jsx
+++ App.jsx
@@ -6,18 +6,32 @@ import { observer } from 'mobx-react'
 class App extends React.Component {
     constructor(props) {
 	   super(props);
-	   this.state = {counter : 0};
-	   this.handleClick = this.handleClick.bind(this);
+	   this.state = {counter : 0, listElement : [], nonStrikeOutCount: 0};
 	   this.textInput = React.createRef();
-	   this.focusTextInput = this.focusTextInput.bind(this);
+	   this.handleClick = this.handleClick.bind(this);
+	   this.listTextInput = this.listTextInput.bind(this);
+	   this.strikeOut = this.strikeOut.bind(this);
    }
    initClickState = new ClickState()
    handleClick() {
        this.setState({counter: this.state.counter + 1})
    }
-   focusTextInput() {
-    this.textInput.current.focus();
-  }
+   listTextInput() {
+	if(this.textInput.current.value != '') {
+		this.setState({listElement: [...this.state.listElement, this.textInput.current.value], nonStrikeOutCount: this.state.nonStrikeOutCount + 1})
+		this.textInput.current.value = '';
+	}
+   }
+   strikeOut(e) {
+	   if(e.target.style.textDecoration != 'line-through') {
+		e.target.style.textDecoration = 'line-through';
+		this.setState({nonStrikeOutCount: this.state.nonStrikeOutCount - 1})
+	   }
+	   else {
+		e.target.style.textDecoration = 'unset';
+		this.setState({nonStrikeOutCount: this.state.nonStrikeOutCount + 1})
+	   }
+   }
    render() {
     const {
         state = this.initClickState
@@ -30,7 +44,7 @@ class App extends React.Component {
         two,
         ...remainingProps
     } = this.props
-    
+    const list = this.state.listElement.map((item) => <li onClick={this.strikeOut}>{item}</li>)
     console.log(remainingProps)
     console.log('Click toggle using MobX', state.isClicked)
       return (
@@ -40,8 +54,12 @@ class App extends React.Component {
 			 </div>
 			 <button onClick={this.handleClick}>Incrementer using setState</button>
 			 <input type="text" ref={this.textInput} />
-			 <input type="button" value="Focus the text Input using ref" onClick={this.focusTextInput} />
+			 <input type="button" value="Focus the text Input using ref" onClick={this.listTextInput} />
 			 <button onClick={state.handleClick}>{'MobX toggle '+state.isClicked}</button>
+			 <ul>
+				{list}
+			 </ul>
+			 <div>{this.state.nonStrikeOutCount} out of {this.state.listElement.length}</div>
 		 </React.Fragment>
       );
    }
